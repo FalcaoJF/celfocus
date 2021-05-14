fc9135bd762f5ec0e0855090cd81f741ddde5c11
Rodrigo Falcão <rodrigofguimaraes@gmail.com>
2021-05-14 20:28:27 -0300

    Finalizada aplicação, o tempo foi corrido, não consegui fazer as classes de teste, mas está funcionando.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,19 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        if(args.length == 0){
+            System.out.println("This application needs an input file to run properly.");
+            return;
+        }
         Map<Integer, String> countryCodes = countryImporter();
         Map<String, Integer> answer = new HashMap<>();
         String line;
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Rodrigo\\repos\\vipyou\\prova\\src\\com\\company\\coutryCodes.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
             while((line = reader.readLine()) != null){
                 if(line.length() < 4){
                     continue;
@@ -21,49 +25,51 @@ public class Main {
                     }
                     answer.put("Portugal",answer.get("Portugal")!= null ? answer.get("Portugal")+1 : 1);
                 } else if (line.length() >=9 && line.substring(0,2).matches("(^\\+?[1-9]{1,3})|((^[0]{2}))")){//Long Number
-                    String digitsOnly = line.replaceAll("[^\\D]","");
-                    digitsOnly = "00".equals(digitsOnly.substring(0,1)) ? digitsOnly.substring(2): digitsOnly;
+                    String digitsOnly = line.replaceAll("[\\D]","");
+                    if(digitsOnly.length()<9) continue;
+                    digitsOnly = "00".equals(digitsOnly.substring(0,2)) ? digitsOnly.substring(2): digitsOnly;
                     if(digitsOnly.length() >=9 && digitsOnly.length() <= 14){
-                        final Integer countryCode1 = new Integer(digitsOnly.substring(0,0));
-                        final Integer countryCode2 = new Integer(digitsOnly.substring(0,1));
-                        final Integer countryCode3 = new Integer(digitsOnly.substring(0,2));
-
+                        final Integer countryCode1 = new Integer(digitsOnly.substring(0,1));
+                        final Integer countryCode2 = new Integer(digitsOnly.substring(0,2));
+                        final Integer countryCode3 = new Integer(digitsOnly.substring(0,3));
                        Optional<Integer> countryCode = countryCodes.keySet().stream().filter(code -> code.equals(countryCode1) || code.equals(countryCode2) || code.equals(countryCode3)).findFirst();
-                       if(countryCode.isPresent()){
-                           answer.put(countryCodes.get(countryCode.get()),answer.get(countryCodes.get(countryCode.get())!= null ? countryCodes.get(countryCode.get())+1 : 1));
-                       }
+                       countryCode.ifPresent(integer -> answer.put(countryCodes.get(integer), answer.get(countryCodes.get(integer)) != null ? answer.get(countryCodes.get(integer)) + 1 : 1));
                     }
                 }
             }
-
+            System.out.println(sortByValue(answer));
         } catch (FileNotFoundException e) {
-            System.out.println("coutryCodes.txt not found. It need to be placed in the same directory of the jar file.");
+            System.out.println("Input file not found.");
         }catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 
     private static Map<Integer, String> countryImporter() {
         Map<Integer, String> countryCodes = new HashMap<>();
         String line;
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Rodrigo\\repos\\vipyou\\prova\\src\\com\\company\\coutryCodes.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader("coutryCodes.txt"));
             while((line = reader.readLine()) != null){
                 String[] country = line.split("-",2);
                 Integer key = Integer.valueOf(country[1]);
                 String value = country[0];
                 countryCodes.put(key,value);
             }
-            System.out.println(countryCodes);
-
         } catch (FileNotFoundException e) {
             System.out.println("coutryCodes.txt not found. It need to be placed in the same directory of the jar file.");
         }catch (IOException e) {
             e.printStackTrace();
         }
         return countryCodes;
+    }
+
+    private static Map<String, Integer> sortByValue(final Map<String, Integer> map) {
+
+        return map.entrySet()
+                .stream()
+                .sorted((Map.Entry.<String, Integer>comparingByValue().reversed()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
     }
 
 
